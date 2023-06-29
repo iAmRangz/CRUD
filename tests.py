da7c6256f52fb527bae7d02b390e668fb44547da
iAmRangz <superango2012@gmail.com>
2023-06-29 17:51:00 +0800

    Add unit tests for crud app

---
+++ tests.py
@@ -0,0 +1,26 @@
+import unittest
+import warnings
+from crud_app import app
+
+
+class MyAppTests(unittest.TestCase):
+    def setUp(self):
+        app.config["TESTING"] = True
+        self.app = app.test_client()
+
+        warnings.simplefilter("ignore", category=DeprecationWarning)
+
+        
+    def test_index_page(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Blank Homepage muna!", response.data)
+        
+
+    def test_get_all_employees(self):
+        response = self.app.get("/employees")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Georgi", response.data)
+
+if __name__ == "__main__":
+    unittest.main()
