c3027fa4b24462aba366e2e68fab519f50a5a101
iAmRangz <202180029@psu.palawan.edu.ph>
2023-07-01 10:56:23 +0800

    Add route to find the department of employee

--- crud_app.py
+++ crud_app.py
@@ -18,6 +18,7 @@ mysql = MySQL(app)
 def index():
     return "Blank Homepage muna!"
 
+
 @app.route("/employees", methods=["GET"])
 def get_all_employees():
     cur = mysql.connection.cursor()
@@ -26,6 +27,7 @@ def get_all_employees():
     cur.close()
     
     return make_response(jsonify(rv), 200)
+
 
 @app.route("/employees/<emp_no>", methods=["GET"])
 def get_employee_by_employee_number(emp_no):
@@ -37,6 +39,20 @@ def get_employee_by_employee_number(emp_no):
     return make_response(jsonify(rv), 200)
 
 
+@app.route("/employees/<emp_no>/department", methods=["GET"])
+def get_employee_department(emp_no):
+    cur = mysql.connection.cursor()
+    cur.execute("""
+        SELECT e.emp_no, e.first_name, e.last_name, d.dept_name
+        FROM employees e
+        JOIN dept_emp de ON e.emp_no = de.emp_no
+        JOIN departments d ON de.dept_no = d.dept_no
+        WHERE e.emp_no = %s AND de.to_date > NOW()
+        """, [emp_no])
+    rv = cur.fetchall()
+    cur.close()
+
+    return make_response(jsonify(rv), 200)
 
 if __name__ == "__main__":
     app.run(debug=True)
