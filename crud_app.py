63452891f9406154525084e1d32bccdb1660e6d9
iAmRangz <202180029@psu.palawan.edu.ph>
2023-07-01 10:58:01 +0800

    Add route to find all the managers of a department

--- crud_app.py
+++ crud_app.py
@@ -54,5 +54,22 @@ def get_employee_department(emp_no):
 
     return make_response(jsonify(rv), 200)
 
+
+@app.route("/departments/<dept_no>/managers", methods=["GET"])
+def get_department_managers(dept_no):
+    cur = mysql.connection.cursor()
+    cur.execute("""
+        SELECT d.dept_no, d.dept_name, e.emp_no, e.first_name, e.last_name
+        FROM dept_manager dm
+        JOIN departments d ON dm.dept_no = d.dept_no
+        JOIN employees e ON dm.emp_no = e.emp_no
+        WHERE d.dept_no = %s
+        """, [dept_no])
+    rv = cur.fetchall()
+    cur.close()
+
+    return make_response(jsonify(rv), 200)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
