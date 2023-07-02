b99fbc448e00fb27537310bf3dc720f5e65f4da8
iAmRangz <202180029@psu.palawan.edu.ph>
2023-07-02 11:02:54 +0800

    Add unit test to get employee by employee number

--- tests.py
+++ tests.py
@@ -22,5 +22,11 @@ class MyAppTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Georgi", response.data)
 
+    
+    def test_get_employee_by_employee_number(self):
+        response = self.app.get("/employees/10069")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Margareta", response.data)
+
 if __name__ == "__main__":
     unittest.main()
