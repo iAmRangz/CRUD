4e3bde7a5fb7039f450a12af625d637e8747b177
iAmRangz <202180029@psu.palawan.edu.ph>
2023-07-02 10:59:34 +0800

    Refactor DB operations into execute_query()

--- crud_app.py
+++ crud_app.py
@@ -7,13 +7,19 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "12345678ten"
 app.config["MYSQL_DB"] = "employees"
-
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
 
 
-#index page
+def execute_query(query, params=None):
+    cur = mysql.connection.cursor()
+    cur.execute(query, params)
+    rv = cur.fetchall()
+    cur.close()
+    return rv
+
+
 @app.route("/")
 def index():
     return "Blank Homepage muna!"
@@ -21,68 +27,48 @@ def index():
 
 @app.route("/employees", methods=["GET"])
 def get_all_employees():
-    cur = mysql.connection.cursor()
-    cur.execute("""SELECT * FROM employees""")
-    rv = cur.fetchall()
-    cur.close()
-    
+    rv = execute_query("SELECT * FROM employees")
     return make_response(jsonify(rv), 200)
 
 
 @app.route("/employees/<emp_no>", methods=["GET"])
 def get_employee_by_employee_number(emp_no):
-    cur = mysql.connection.cursor()
-    cur.execute("""SELECT * FROM employees WHERE emp_no = %s""", (emp_no,))
-    rv = cur.fetchall()
-    cur.close()
-
+    rv = execute_query("SELECT * FROM employees WHERE emp_no = %s", (emp_no,))
     return make_response(jsonify(rv), 200)
 
 
 @app.route("/employees/<emp_no>/department", methods=["GET"])
 def get_employee_department(emp_no):
-    cur = mysql.connection.cursor()
-    cur.execute("""
+    rv = execute_query("""
         SELECT e.emp_no, e.first_name, e.last_name, d.dept_name
         FROM employees e
         JOIN dept_emp de ON e.emp_no = de.emp_no
         JOIN departments d ON de.dept_no = d.dept_no
         WHERE e.emp_no = %s AND de.to_date > NOW()
         """, [emp_no])
-    rv = cur.fetchall()
-    cur.close()
-
     return make_response(jsonify(rv), 200)
 
 
 @app.route("/departments/<dept_no>/managers", methods=["GET"])
 def get_department_managers(dept_no):
-    cur = mysql.connection.cursor()
-    cur.execute("""
+    rv = execute_query("""
         SELECT d.dept_no, d.dept_name, e.emp_no, e.first_name, e.last_name
         FROM dept_manager dm
         JOIN departments d ON dm.dept_no = d.dept_no
         JOIN employees e ON dm.emp_no = e.emp_no
         WHERE d.dept_no = %s
         """, [dept_no])
-    rv = cur.fetchall()
-    cur.close()
-
     return make_response(jsonify(rv), 200)
 
 
 @app.route("/employees/<emp_no>/salaries", methods=["GET"])
 def get_employee_salaries(emp_no):
-    cur = mysql.connection.cursor()
-    cur.execute("""
+    rv = execute_query("""
         SELECT e.emp_no, e.first_name, e.last_name, s.salary, s.from_date, s.to_date
         FROM employees e
         JOIN salaries s ON e.emp_no = s.emp_no
         WHERE e.emp_no = %s
         """, [emp_no])
-    rv = cur.fetchall()
-    cur.close()
-
     return make_response(jsonify(rv), 200)
 
 
